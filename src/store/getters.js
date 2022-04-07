e9e3a8f4eb488acd512a6d25f72cff9a280d9fb5
usamihal <usamihal001@163.com>
2022-04-07 21:49:01 +0800

    feat: 渲染用户头像

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,5 +2,6 @@ export default {
   token: state => state.user.token,
   hasUserInfo: state => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: state => state.user.userInfo
 }
