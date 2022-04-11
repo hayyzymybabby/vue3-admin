563bcd1946cd3dfec0d78215599b14825a72ef97
usamihal <usamihal001@163.com>
2022-04-11 21:28:01 +0800

    style: SidebarHeader 处理

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="">
-    <h1>111</h1>
+    <div class="logo-container">
+      <el-avatar
+        size="44"
+        shape="square"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+      ></el-avatar>
+      <h1 class="logo-title" v-if="$store.getters.sidebarOpened">
+        imooc-admin
+      </h1>
+    </div>
+
     <el-scrollbar>
       <SidebarMenu></SidebarMenu>
     </el-scrollbar>
@@ -12,4 +22,20 @@ import SidebarMenu from './SidebarMenu'
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
